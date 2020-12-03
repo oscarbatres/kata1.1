447b6dea56079a8aff7a7ccdc486d53afe196a60
Oscar <obatres97@gmail.com>
2020-12-03 19:22:45 +0000

    second version of Kata1

---
+++ src/main/java/kata1_2/Person.java
@@ -0,0 +1,30 @@
+package kata1_2;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Person {
+    private final String name;
+    private final Calendar birthdate;
+    private final long MILLISECONDS_PER_YEAR = (long) (1000*60*60*24*365.25);
+
+    public Person(String name, Calendar birthdate) {
+        this.name = name;
+        this.birthdate = birthdate;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public int getAge(){
+        Calendar today = GregorianCalendar.getInstance();
+        return (int)  (milliSecondsToYear(today.getTimeInMillis() - birthdate.getTimeInMillis()));
+    }
+    
+    private long milliSecondsToYear (long milles){
+        return milles/MILLISECONDS_PER_YEAR;
+    }
+
+}
